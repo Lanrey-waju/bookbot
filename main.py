d87f8d5af7af38b719b3e57a0b748d36353e83a1
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-02-01 20:28:33 +0100

    add code to compute word count of the book

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ def get_book_text(path):
 
 
 def main():
-    content = get_book_text("books/frankenstein.txt")
-    print(content)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_of_words = count_words(text)
+    print(f"There are {num_of_words} words in this book.")
+
+
+def count_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
 
 
 main()
