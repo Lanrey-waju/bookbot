34e1d9a881d44e38ff01654fb782fa85d2c4d69e
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-02-01 23:25:55 +0100

    add code to count the characters in the text and return in a dictionary

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_of_words = count_words(text)
-    print(f"There are {num_of_words} words in this book.")
+    letter_count = count_letters(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 def count_words(text):
     words = text.split()
     word_count = len(words)
     return word_count
+
+
+def count_letters(text):
+    count = {}
+    for char in text.lower():
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 0
+    return count
 
 
 main()
